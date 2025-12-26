aa72c91eaa71783166fff1679973c95973b662ff
Radu-A <elcastravete@gmail.com>
2025-12-26 15:07:29 +0100

    chore: create folder structure and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Characters } from './pages/characters/characters';
+import { Details } from './pages/details/details';
+import { Generator } from './pages/generator/generator';
 
-export const routes: Routes = [];
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: Characters,
+    title: 'Characters',
+  },
+  {
+    path: 'details',
+    component: Details,
+    title: 'Details',
+  },
+  {
+    path: 'generator',
+    component: Generator,
+    title: 'Generator',
+  },
+];
