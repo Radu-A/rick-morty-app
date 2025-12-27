ca7e075c9ba248fd24f0b0bd9644bf60c6cd73f8
Radu-A <elcastravete@gmail.com>
2025-12-27 12:08:41 +0100

    feat(characters): add for of with async pipe to render character cards

--- src/app/pages/characters/characters.ts
+++ src/app/pages/characters/characters.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { CharacterService } from '../../services/character.service';
+import { CharacterCard } from '../../shared/character-card/character-card';
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  imports: [AsyncPipe, CharacterCard],
   templateUrl: './characters.html',
   styleUrl: './characters.css',
 })
 export class Characters {
-
+  characterService = inject(CharacterService);
+  characterList$ = this.characterService.getAllCharacters();
 }
