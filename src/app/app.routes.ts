d077b056dc3594e58cea8bd58f1a2abd303a2f03
Radu-A <elcastravete@gmail.com>
2026-01-01 14:29:26 +0100

    feat(locations): add locations page with locations card grid and search input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Characters } from './pages/characters/characters';
 import { Details } from './pages/details/details';
+import { Locations } from './pages/locations/locations';
 import { Generator } from './pages/generator/generator';
 
 export const AppRoutes: Routes = [
@@ -8,6 +9,11 @@ export const AppRoutes: Routes = [
     path: '',
     component: Characters,
     title: 'Characters',
+  },
+  {
+    path: 'locations',
+    component: Locations,
+    title: 'Locations',
   },
   {
     path: 'details/:id',
