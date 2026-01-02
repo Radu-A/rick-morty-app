aa64be3d1dd6da4e043ebbea90ce2be441e5e9da
Radu-A <elcastravete@gmail.com>
2026-01-02 13:39:59 +0100

    feat(characters): add characters component, routes and service

--- src/app/shared/character-card/character-card.ts
+++ src/app/shared/character-card/character-card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CharacterInterface } from '../../models/character.model';
 
 @Component({
   selector: 'app-character-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './character-card.html',
   styleUrl: './character-card.css',
 })
