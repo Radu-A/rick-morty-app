a281580156bd17a36f7ba3943fd8691419465959
Radu-A <elcastravete@gmail.com>
2026-01-02 13:29:35 +0100

    feat(header): add locations route to router and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Characters } from './pages/characters/characters';
 import { Details } from './pages/details/details';
+import { Locations } from './pages/locations/locations';
+import { LocationDetails } from './pages/location-details/location-details';
 import { Generator } from './pages/generator/generator';
 
 export const AppRoutes: Routes = [
@@ -10,9 +12,19 @@ export const AppRoutes: Routes = [
     title: 'Characters',
   },
   {
-    path: 'details',
+    path: 'details/:id',
     component: Details,
     title: 'Details',
+  },
+  {
+    path: 'locations',
+    component: Locations,
+    title: 'Locations',
+  },
+  {
+    path: 'location-details/:id',
+    component: LocationDetails,
+    title: 'Location Details',
   },
   {
     path: 'generator',
